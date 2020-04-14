4b0437165cb3d4dba444ecfb6eacc3bb9dbc56a5
DinuGarbuz <DinuGarbuz@users.noreply.github.com>
2020-04-14 09:47:08 +0300

    Tema1

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {           
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+            Console.WriteLine("Name: ");
+            var input = Console.ReadLine();
+            var clientRequested = new CustomerLookupModel { Name = input.ToString() };
+            var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+        }
+    }
+}
---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            Console.WriteLine("Hello, " + request.Name);
+
+            return Task.FromResult(new CustomerModel { });
+         
+        }
+    }
+}
